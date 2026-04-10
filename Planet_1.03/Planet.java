5da77e74651e23be7db787a7aaa2ceaf0a287932
philippt07 <philipp.thieben@windthorstgymnasium.de>
2026-04-10 12:12:03 +0200

    1.Test

--- Planet_1.03/Planet.java
+++ Planet_1.03/Planet.java
@@ -28,5 +28,37 @@ public class Planet extends World
      */
     private void prepare()
     {
+        Rover rover = new Rover();
+        addObject(rover,2,1);
+        Rover rover2 = new Rover();
+        addObject(rover2,2,2);
+        Rover rover3 = new Rover();
+        addObject(rover3,2,3);
+        Rover rover4 = new Rover();
+        addObject(rover4,2,4);
+        Rover rover5 = new Rover();
+        addObject(rover5,2,5);
+        Rover rover6 = new Rover();
+        addObject(rover6,2,6);
+        Rover rover7 = new Rover();
+        addObject(rover7,5,1);
+        Rover rover8 = new Rover();
+        addObject(rover8,5,2);
+        Rover rover9 = new Rover();
+        addObject(rover9,5,3);
+        Rover rover10 = new Rover();
+        addObject(rover10,5,4);
+        Rover rover11 = new Rover();
+        addObject(rover11,5,5);
+        Rover rover12 = new Rover();
+        addObject(rover12,5,6);
+        Rover rover13 = new Rover();
+        addObject(rover13,2,7);
+        Rover rover14 = new Rover();
+        addObject(rover14,5,7);
+        Rover rover15 = new Rover();
+        addObject(rover15,3,4);
+        Rover rover16 = new Rover();
+        addObject(rover16,4,4);
     }
 }
